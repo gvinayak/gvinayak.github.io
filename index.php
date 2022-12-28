cca04906e05a1abaef8c83ff1f06ff3085f4a276
gvinayak <guptavinayak51@gmail.com>
2022-12-28 23:48:55 +0530

    Export to php

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Vinayak Gupta</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Research Scientist at IBM">
+	<link rel="shortcut icon" href="pand.ico">  
+
+	<!-- Google Font -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+	<!-- Global CSS -->
+	<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+	<!-- Theme CSS -->  
+	<link id="theme-style" rel="stylesheet" href="assets/css/orbit-6.css">
+</head> 
+
+<body>
+<div class="wrapper mt-lg-5">
+<?php include 'right_col.php';?>
+		
+<div class="main-wrapper">
+	<section class="section summary-section">
+		<h2 class="section-title"><span class="icon-holder"><i class="fas fa-user"></i></span>About Me</h2>
+		<div class="summary">
+			<p align="justify"> I am an AI Research Scientist at <a href="https://research.ibm.com/">IBM Research</a>, New Delhi, India. I completed my Ph.D. from the <a href="https://www.cse.iitd.ac.in/">Department of Computer Science</a> at the Indian Institute of Technology Delhi. At IIT-D, I was fortunate to be advised by <a href="http://www.cse.iitd.ac.in/~srikanta/">Prof. Srikanta Bedathur</a> and to have collaborated with <a href="http://www.cse.iitb.ac.in/~abir/">Prof. Abir De</a>. </p>
+
+			<p align="justify">My research interests are in machine learning. In particular, I work on deep generative, graph-based models, and temporal point processes, with applications in sequential data mining, missing-data imputation, spatio-temporal prediction, and community detection. Prior to this, I received my bachelor's in Computer Science and Engineering from the <a href="https://www.iiitdmj.ac.in/">Indian Institute of Information Technology (IIIT) Jabalpur</a>. I have also worked as an Applied Scientist-Intern at <a href="https://www.amazon.science/">Amazon</a>, and as an R&D-Intern at <a href="https://www.siemens-healthineers.com/en-in/careers/innovation-think-tank">Siemens Healthineers</a>.</p>
+		</div>
+	</section>
+
+	<section class="section projects-section">
+		<h2 class="section-title"><span class="icon-holder"><i class="fas fa-newspaper"></i></span>News</h2>
+		<ul>
+			<li><b>[Oct '22]</b> <a href="https://indiaai.gov.in/article/read-and-watch-lectures-to-build-a-foundation"><u>Expert Talk</u></a> at IndiaAI! Organized by NASSCOM and Ministry of IT, Govt. of India.</li>
+			<li><b>[Oct '22]</b> Presented my research at GeorgiaTech and Univ. of Michigan, Ann Arbor. </li>
+			<li><b>[Aug '22]</b> Big move! Joined <b>IBM Research</b>, India!</li>
+			<li><b>[Jun '22]</b> Adjudged as <a href="https://indiaai.gov.in/research-reports/ai-gamechangers-2022-realizing-india-s-ai-promise"><u>Challenger</u></a> (Top 3) in NASSCOM AI Gamechangers Awards 2022!</li>
+			<li><b>[May '22]</b> Paper accepted in <b>KDD 2022</b>!</li>
+			<li><b>[Dec '21]</b> Paper accepted in <b>AAAI 2022</b>!</li>
+			<li><b>[Nov '21]</b> Presented my work at <b>Amazon Research Days 2021</b>.</li>
+			<li><b>[Oct '21]</b> My <a href="https://arxiv.org/abs/2111.07189"><u>work</u></a> received the <b>Outstanding Doctoral Paper</b> award at <b><a href="https://www.aimlsystems.org/2021/">AI-ML Systems 2021</a></b>!</li>
+		</ul>
+	</section>
+
+	<section class="section experiences-section">
+		<h2 class="section-title"><span class="icon-holder"><i class="fas fa-briefcase"></i></span>Industry Experience</h2>
+		<div class="item">
+		<div class="meta">
+			<div class="upper-row">
+				<h3 class="job-title">IBM Research</h3>
+				<div class="time">Aug 2022 - Present</div>
+			</div>
+			<div class="company">Research Scientist</div>
+		</div>
+		<div class="details">
+			<p align="justify">Working as a Research Scientist at IBM India Research Lab (IRL), New Delhi. Our goal is to understand the properties that are necessary for an automated business intelligence system. </br>
+		</div>
+		</div>
+
+		<div class="item">
+		<div class="meta">
+			<div class="upper-row">
+				<h3 class="job-title">Amazon</h3>
+				<div class="time">Jan 2022 - Jun 2022</div>
+			</div>
+			<div class="company">Applied Scientist Intern</div>
+		</div>
+		<div class="details">
+			<p align="justify">I worked with the ML team on time-sensitive reward distribution for users in Amazon Pay using their transactions and purchases. The project involved learning the temporal relations between different user payment records.</br>
+		</div>
+		</div>
+
+		<div class="item">
+		<div class="meta">
+			<div class="upper-row">
+				<h3 class="job-title">Siemens Healthineers AG</h3>
+				<div class="time">May 2016 - Jan 2017</div>
+			</div>
+			<div class="company">Research Intern</div>
+		</div>
+		<div class="details">
+			<p align="justify">I worked on designing ML models for improving image quality during radiography and fluoroscopy in Siemens X-ray devices. I also contributed to the initial phase of a project to design a probe for autonomous ultrasonography during telemedicine.</br>
+			<b>Tech-Intern Rating: 1 </b>(Highest Possible)</p>
+		</div>
+		</div>
+	</section>
+
+	<section class="section publications-section">
+		<h2 class="section-title"><span class="icon-holder"><i class="fas fa-rocket"></i></span>Selected Publications (<a href="all_pubs.php"><u>View All</u></a>)</h2>
+		<div class="intro"></div>
+		<div class="item">
+			<div class="publication-title">ProActive: Self-Attentive Temporal Point Process Flows for Activity Sequences.</div>
+			<div class="publication-authors"><strong>Vinayak Gupta</strong> and Srikanta Bedathur.</div>
+			<div class="publication-conference"><i>ACM SIGKDD Conference on Knowledge Discovery and Data Mining (<b>KDD</b>)</i>, 2022.</div>
+			<div class="publication-content"><a href="https://dl.acm.org/doi/10.1145/3534678.3539477">[<u>Paper</u>]</a>&nbsp&nbsp<a href="https://github.com/data-iitd/proactive">[<u>Code</u>]</a></div>
+		</div>
+
+		<div class="item">
+			<div class="publication-title">Learning Temporal Point Processes for Efficient Retrieval of Continuous Time Event Sequences.</div>
+			<div class="publication-authors"><strong>Vinayak Gupta</strong>, Srikanta Bedathur, and Abir De.</div>
+			<div class="publication-conference"><i>AAAI Conference on Artificial Intelligence (<b>AAAI</b>)</i>, 2022.</div>
+			<div class="publication-content"><a href="https://ojs.aaai.org/index.php/AAAI/article/view/20317">[<u>Paper</u>]</a>&nbsp&nbsp<a href="https://github.com/data-iitd/neuroseqret", target="_blank">[<u>Code</u>]</a></div>
+		</div>
+
+		<div class="item">
+			<div class="publication-title">Doing More with Less: Overcoming Data Scarcity for POI Recommendation via Cross-Region Transfer.</div>
+			<div class="publication-authors"><strong>Vinayak Gupta</strong> and Srikanta Bedathur.</div>
+			<div class="publication-conference"><i>ACM Transactions on Intelligent Systems and Technology (<b>TIST</b>)</i>, 2022.</div>
+			<div class="publication-content"><a href="https://arxiv.org/abs/2201.06095">[<u>Paper</u>]</a>&nbsp&nbsp<a href="#">[<u>Code</u>]</a></div>
+		</div>
+
+		<div class="item">
+			<div class="publication-title">Learning Temporal Point Processes with Intermittent Observations.</div>
+			<div class="publication-authors"><strong>Vinayak Gupta</strong>, Srikanta Bedathur, Sourangshu Bhattacharya, and Abir De.</div>
+			<div class="publication-conference"><i>International Conference on Artificial Intelligence and Statistics (<b>AISTATS</b>)</i>, 2021.</div>
+			<div class="publication-content"><a href="http://proceedings.mlr.press/v130/gupta21a.html">[<u>Paper</u>]</a>&nbsp&nbsp<a href="https://github.com/data-iitd/imtpp", target="_blank">[<u>Code</u>]</a></div>
+		</div>
+
+		<div class="item">
+			<div class="publication-title">Region Invariant Normalizing Flows for Mobility Transfer.</div>
+			<div class="publication-authors"><strong>Vinayak Gupta</strong> and Srikanta Bedathur.</div>
+			<div class="publication-conference"><i>ACM International Conference on Information and Knowledge Management (<b>CIKM</b>)</i>, 2021.</div>
+			<div class="publication-content"><a href="https://dl.acm.org/doi/abs/10.1145/3459637.3482169">[<u>Paper</u>]</a></a>&nbsp&nbsp<a href="https://github.com/data-iitd/reformd", target="_blank">[<u>Code</u>]</a></div>
+		</div>
+
+	</section>
+	<!-- <section class="section projects-section">
+		<h2 class="section-title"><span class="icon-holder"><i class="fas fa-archive"></i></span>Under Review</h2>
+		<div class="intro">
+			<p>Too many to be updated! Will do it once they're accepted. </p>
+		</div> -->
+		<!--	<div class="item">
+			<span class="project-title"><a href="#" target="_blank">CoderPro</a></span> - <span class="project-tagline">A responsive website template designed to help developers launch their software projects. </span>
+		</div> -->
+	<!-- </section> -->
+	
+	<section class="section projects-section">
+		<h2 class="section-title"><span class="icon-holder"><i class="fas fa-paw"></i></span>About Me (The real one!)</h2>
+		<div class="intro">
+		<p align="justify">
+			Wow! You're still reading? Since you've come all the way here, let me tell you some interesting stuff about me. I grew up entirely in Delhi (the city of Smoke &#128517;) but was born in Allahabad (the city of <a href="https://www.google.com/search?q=which+city+is+known+as+the+city+of+prime+minister">Prime Ministers</a> &#128526;). Before I went on with my seemingly never-ending degree-grabbing spree, I was a dorky student at <a href="https://www.dpsindirapuram.com/">DPS Indirapuram</a> (a Dipsite!), an even dorkier student at <a href="https://noida.cambridgeschool.edu.in/">Cambridge School</a> Noida, and even dorkier at the school before (got the point?).
+		</p>
+		<p align="justify">
+			During my amazing four years as an undergrad, I was a member of the <a href="https://en.wikipedia.org/wiki/ABU_Robocon">ABU Robocon</a> team that was placed 9th all over India (our bots played badminton!), a rhythm guitarist in the institute rock band, a member of the counseling committee (never did anything here), and organizer of many unsung fests and clubs. I have, on occasion, also been known to do something useful there such as designing sensor network protocols and as an assistant for maintaining the networking and internet infra of the entire campus.
+		</p>
+		</div>
+	</section>
+			
+</div>
+</div>
+<?php include 'footer.php';?>
+</body>
+</html>
